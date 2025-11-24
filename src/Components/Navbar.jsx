ca2925a239f417c8de6893e25aa7fca941457669
Rishabhxavier84 <rishabhxavier84@gmail.com>
2025-11-24 15:34:02 +0530

    Implemented Nav bar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import dayjs from "dayjs";
+import { navIcons, navLinks } from "#constants";
+import React from "react";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav>
+      <div>
+        <img src="images/logo.svg" alt="logo" />
+        <p className="font-bold text-sm">Rishabh's Portfolio</p>
 
-export default Navbar
+        <ul>
+          {navLinks.map(({ id, name, type }) => (
+            <li key={id}>
+              <p>{name}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div>
+        <ul>
+          {navIcons.map(({ id, img }) => (
+            <li key={id}>
+              <img src={img} alt={`icon-${id}`} className="icon" />
+            </li>
+          ))}
+        </ul>
+
+        <time>{dayjs().format("ddd D MMM h:mmA")}</time>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
